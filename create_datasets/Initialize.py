4e631b7729eb38e8cd732a852b90361e8c5fb3ec
BenM61 <benmassarano4@gmail.com>
2022-02-28 17:29:45 +0200

    added copy_then_preprocess_song

--- create_datasets/Initialize.py
+++ create_datasets/Initialize.py
@@ -1,13 +1,14 @@
-import create_db.config
+import create_db.config as db_config
 import config
 import TokenizedLyricsDataset
+from preprocessing import preprocess
 
 import os
 import random
 from pathlib import Path
 import shutil
 
-def copy_song(from_path, root):
+def copy_then_preprocess_song(from_path, root):
   from_path = str(from_path)
   name = from_path.split(os.path.sep)[-1]
   folder = from_path.split(os.path.sep)[-2]
@@ -16,6 +17,7 @@ def copy_song(from_path, root):
 
   Path.mkdir(folder_path, parents=True, exist_ok=True)
   shutil.copy(from_path, to_path)
+  preprocess(to_path)
 
 def create_datasets():
   paths = []
@@ -24,9 +26,9 @@ def create_datasets():
     folder = os.path.join(root, folder_name)
     if os.path.isdir(folder):
       for filename in os.listdir(folder):
-          file_path = os.path.join(folder, filename)
-          if os.path.isfile(file_path):
-            paths.append(file_path)
+        file_path = os.path.join(folder, filename)
+        if os.path.isfile(file_path):
+          paths.append(file_path)
 
   random.Random(666).shuffle(paths)
   split_ind = int(len(paths) * config.TRAIN_TEST_SPLIT)
@@ -34,13 +36,14 @@ def create_datasets():
   test_paths = paths[split_ind:]
 
   for path in train_paths:
-    copy_song(path, config.TRAIN_PATH)
-    exit(4)
+    copy_then_preprocess_song(path, config.TRAIN_PATH)
 
   for path in test_paths:
-    copy_song(path, config.TEST_PATH)
+    copy_then_preprocess_song(path, config.TEST_PATH)
 
   train_dataset = TokenizedLyricsDataset(train_paths, True)
   test_dataset = TokenizedLyricsDataset(test_paths, False)
 
   return train_dataset, test_dataset
+
+create_datasets()
--- create_datasets/preprocessing.py
+++ create_datasets/preprocessing.py
@@ -1,119 +1,97 @@
 import os
 
-def preprocess():
-    for i in range(1,8):
-        print(i)
-        genre=""
-        if (i==1):
-            genre = "BLUES"
-        if (i==2):
-            genre = "ELECTRONIC"
-        if (i==3):
-            genre = "HIP_HOP"
-        if (i==4):
-            genre = "POP"
-        if (i==5):
-            genre = "ROCK"
-        if (i==6):
-            genre = "FUNK"
-        if (i==7):
-            genre = "JAZZ"  
-        
-        directory = "create_db/DB/"+ genre
-        for song in os.listdir(directory):
-            #song.txt
-            with open(directory+"/"+song, "r") as f:
-                print(song)
-                lines = f.readlines()
-            if (len(lines) == 0):
-                continue
-            chorDec = True
-            isChorus = False
-            isRep = False
-            chor =[]
-            repBlock = []
-            new_song = []
-            repNum = 0
-            if lines[-1] != "\n":
-                lines.append("\n")
+def preprocess(song_path):
+    with open(song_path, "r") as f:
+        lines = f.readlines()
+    if (len(lines) == 0):
+        return
+    chorDec = True
+    isChorus = False
+    isRep = False
+    chor =[]
+    repBlock = []
+    new_song = []
+    repNum = 0
+    if lines[-1] != "\n":
+        lines.append("\n")
 
-            for line in lines:
-                line = line.replace("[","(").replace("]",")")
-                line = line.replace("..","...")
-                while True:
-                    if ("...." in line):
-                        line = line.replace("....","...")
-                    else:
-                        break
+    for line in lines:
+        line = line.replace("[","(").replace("]",")")
+        line = line.replace("..","...")
+        while True:
+            if ("...." in line):
+                line = line.replace("....","...")
+            else:
+                break
 
-                l = line.replace("\n","").replace("(","").strip().upper()
+        l = line.replace("\n","").replace("(","").strip().upper()
 
-                #[chorus:]
-                if l.startswith("CHORUS") or l.startswith("REPEAT CHORUS"):
-                    if chorDec: #first time we seeing the chorus
-                        isChorus = True
-                        chorDec = False
-                    else: #repeat chorus
-                        repeat = 1
-                        if "X" in l or "REPEAT" in l:
-                            for letter in l:
-                                if letter.isdigit():
-                                    repeat = max(int(letter)-1,1)
-                        for j in range(0,repeat):
-                            for c in chor:
-                                new_song.append(c)
-                            if j != repeat-1:
-                                new_song.append("\n")
+        #[chorus:]
+        if l.startswith("CHORUS") or l.startswith("REPEAT CHORUS"):
+            if chorDec: #first time we seeing the chorus
+                isChorus = True
+                chorDec = False
+            else: #repeat chorus
+                repeat = 1
+                if "X" in l or "REPEAT" in l:
+                    for letter in l:
+                        if letter.isdigit():
+                            repeat = max(int(letter)-1,1)
+                for j in range(0,repeat):
+                    for c in chor:
+                        new_song.append(c)
+                    if j != repeat-1:
+                        new_song.append("\n")
+        else:
+            repeat = 1
+
+            #[repeat: x3]
+            if "REPEAT" in l:
+                for letter in l:
+                    if letter.isdigit():
+                        repeat = max(int(letter)-1,1)
+
+                line = line.split("[")[0].strip()
+                line = line.split("(Repeat")[0].strip()
+
+                #bla bla bla [repeat: x3]
+                if line != "":
+                    line = line + "\n"
+                    for j in range(0,repeat):
+                        new_song.append(line)
+
+                # [repeat: x4]
+                # bla 
+                # bla
+
                 else:
-                    repeat = 1
+                    isRep = True
+                    repNum = repeat
 
-                    #[repeat: x3]
-                    if "REPEAT" in l:
-                        for letter in l:
-                            if letter.isdigit():
-                                repeat = max(int(letter)-1,1)
+            #regular lines
+            new_song.append(line)
+            if (isChorus):
+                if line == "\n":
+                    isChorus = False
+                else:
+                    chor.append(line)
+            
+            if(isRep):
+                if line == "\n" and len(repBlock)>0:                     
+                    isRep = False
+                    repBlock.append(line)
+                    for j in range (0,repNum):
+                        for b in repBlock:
+                            new_song.append(b)
+                    repBlock = []
+                else:
+                    repBlock.append(line)
 
-                        line = line.split("[")[0].strip()
-                        line = line.split("(Repeat")[0].strip()
-
-                        #bla bla bla [repeat: x3]
-                        if line != "":
-                            line = line + "\n"
-                            for j in range(0,repeat):
-                                new_song.append(line)
-
-                        # [repeat: x4]
-                        # bla 
-                        # bla
-
-                        else:
-                            isRep = True
-                            repNum = repeat
-
-                    #regular lines
-                    new_song.append(line)
-                    if (isChorus):
-                        if line == "\n":
-                            isChorus = False
-                        else:
-                            chor.append(line)
-                    
-                    if(isRep):
-                        if line == "\n" and len(repBlock)>0:                     
-                            isRep = False
-                            repBlock.append(line)
-                            for j in range (0,repNum):
-                                for b in repBlock:
-                                    new_song.append(b)
-                            repBlock = []
-                        else:
-                            repBlock.append(line)
-
-            if (new_song[-2] == "\n"):
-                new_song = new_song[:-1]
-            with open(directory+"/"+song, "w") as f:
-                for line in new_song:
-                    f.write(line)
+    if (new_song[-2] == "\n"):
+        new_song = new_song[:-1]
+    with open(song_path, "w") as f:
+        for line in new_song:
+            f.write(line)
 
 #check there is no text like [chorus] or [repeat] and this song is only lyrics
 # AFTER we have done the preprocess
