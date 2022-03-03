717d9e75aab8ea9a4cbbd8ef9d94748b310645cf
BenM61 <benmassarano4@gmail.com>
2022-03-03 14:33:31 +0200

    undo ignoring titles

--- create_datasets/LyricsDataset.py
+++ create_datasets/LyricsDataset.py
@@ -4,6 +4,9 @@ from torch.utils.data import Dataset
 
 
 class LyricsDataset(Dataset):
+	def _get_title(self, song_path):
+		return str(song_path).split(os.path.sep)[-1][:-4].replace("_", " ")
+
 	def _get_lyrics(self, song_path):
 		with open(song_path, "r") as f:
 			return f.read()
@@ -17,12 +20,15 @@ class LyricsDataset(Dataset):
 		super(LyricsDataset, self).__init__()
 
 		self.num_songs = len(songs_paths)
+		self.titles = []
 		self.lyrics = []
 		self.labels = []
 		for path in songs_paths:
+			title = self._get_title(path)
 			lyrics = self._get_lyrics(path)
 			label = self._get_labels(title_dict, path)
 
+			self.titles.append(title)
 			self.lyrics.append(lyrics)
 			self.labels.append(label)
 
@@ -30,4 +36,4 @@ class LyricsDataset(Dataset):
 		return self.num_songs
 	
 	def __getitem__(self, index):
-		return self.lyrics[index], self.labels[index]
+		return tuple(self.titles[index], self.lyrics[index]), self.labels[index]
